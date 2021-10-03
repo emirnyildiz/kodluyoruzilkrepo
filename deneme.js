e28abeb8c69c4a9d3969b3145dff748fb44de94c
emirnyildiz <emirnuriyildiz@gmail.com>
2021-10-03 20:26:45 +0300

    js deneme

---
+++ deneme.js
@@ -0,0 +1,11 @@
+let username = "emirnyildiz"
+const DOMAIN = "kodluyoruz.org"
+
+let email = username + "@" + DOMAIN
+
+let info = ` Merhaba ${username}. Kodluyoruz.org'a hoşgeldin!
+Bu sitede kullanacak olduğun email adresin: ${email}
+Aylık olarak üyelik ücreti: ${800/12}
+kullanıcı adı uzunluğu = ${username.length}
+Saat: ${new Date().getHours()}
+`
